97819d24961733fa24a8fdae7b61888411267169
VITEK <mixaylow.viktor@yandex.ru>
2019-12-10 12:29:10 +0700

    10/12/19

---
+++ WindowsFormsApp9_АРКАНОИД_ФИНАЛ (10.12.19)/WindowsFormsApp9АРКАНОИД_ФИНАЛ/LOADING.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9АРКАНОИД_ФИНАЛ
+{
+    public partial class LOADING : Form
+    {
+        public LOADING()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            this.Close();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+
+        }
+    }
+}
---
+++ WindowsFormsApp9_АРКАНОИД_ФИНАЛ (10.12.19)/WindowsFormsApp9АРКАНОИД_ФИНАЛ/REGISTRSTION.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp9АРКАНОИД_ФИНАЛ
+{
+    partial class REGISTRSTION
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(REGISTRSTION));
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.PasswordBox.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordBox.Location = new System.Drawing.Point(20, 220);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '*';
+            this.PasswordBox.Size = new System.Drawing.Size(454, 48);
+            this.PasswordBox.TabIndex = 11;
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.LoginBox.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginBox.Location = new System.Drawing.Point(20, 79);
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(454, 48);
+            this.LoginBox.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 40);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 40);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Логин";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(288, 668);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 49);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(20, 668);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Регистрация";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(24, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 40);
+            this.label3.TabIndex = 12;
+            this.label3.Text = ".";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 306);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.BackColor = System.Drawing.Color.SkyBlue;
+            this.LoadImageButton.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoadImageButton.Location = new System.Drawing.Point(20, 589);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(318, 48);
+            this.LoadImageButton.TabIndex = 14;
+            this.LoadImageButton.Text = "Загрузить фото";
+            this.LoadImageButton.UseVisualStyleBackColor = false;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // REGISTRSTION
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(494, 726);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.LoginBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "REGISTRSTION";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Регистрация";
+            this.Load += new System.EventHandler(this.REGISTRSTION_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadImageButton;
+    }
+}
